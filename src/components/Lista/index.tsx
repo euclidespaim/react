a5e7b6726fd098ecc91b492c6bb4497783114549
euclidespaim <euclidespaim@gmail.com>
2022-07-02 12:24:13 -0300

    Function components

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 
 function Lista() {
-    return(
-        <aside>
-
-        </aside>
-    )
+  const tarefas = [
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "JavaScript",
+      tempo: "01:00:00",
+    },
+  ];
+  return (
+    <aside>
+      <h2>Estudos do dia</h2>
+      <ul>
+        
+        <li>
+          <h3>React</h3>
+          <span>02:00:00</span>
+        </li>
+        <li>
+          <h3>JavaScript</h3>
+          <span>01:00:00</span>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
-export default Lista
+export default Lista;

d4e85dbd62c8a6f6696dd2e6d4fca045e659de12
euclidespaim <euclidespaim@gmail.com>
2022-07-02 13:16:47 -0300

    Adicionado scsss

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import './style.scss';
 
 class Botao extends React.Component {
   render() {
-    return <button>Press me!</button>;
+    return (
+      <button className="botao">
+        Press me!
+      </button>
+    )
   }
 }
 

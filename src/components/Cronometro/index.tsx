da2a6b217f76d084d2b986124f7409cee504395d
euclidespaim <euclidespaim@gmail.com>
2022-07-08 11:05:47 -0300

    Commons

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,12 +1,26 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useState } from "react";
 
-export default function Cronometro() {
+interface Props{
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado }: Props) {
+
+    const [tempo, setTempo] = useState<number>();
+    if (selecionado?.tempo) {
+        setTempo(tempoParaSegundos(selecionado?.tempo))
+    }
+
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>
                 Escolha o card e inicie o cronômetro!
+                Tempo: {tempo}
             </p>
             <div className={style.relogioWrapper}>
                 <Relogio />
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -25,7 +25,7 @@ function App() {
         selecionaTarefa={selecionaTarefa}
 
       />
-      <Cronometro />
+      <Cronometro selecionado={selecionado}/>
     </div>
   );
 }
